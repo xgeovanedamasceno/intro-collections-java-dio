34ed2585321076c940596460ad87e36115542b8b
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2023-08-23 17:27:13 -0300

    add methods to test TestList class

--- src/TestList.java
+++ src/TestList.java
@@ -1,5 +1,23 @@
+import list.TaskList;
+
 public class TestList {
     public static void main(String[] args) {
+        TaskList listOne = new TaskList();
 
+        listOne.addTask("Do Dishes");
+        listOne.addTask("Mop the floor");
+        listOne.addTask("Make the bed");
+        listOne.addTask("Cook the dinner");
+
+        System.out.println("**");
+        System.out.println(listOne.getNumberOfTask());
+        listOne.getTaskDescriptions();
+
+
+        listOne.removeTask("Make the bed");
+
+        System.out.println("**");
+        System.out.println(listOne.getNumberOfTask());
+        listOne.getTaskDescriptions();
     }
 }
